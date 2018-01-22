14277406aa9c461947675776881725b4bc01d2f2
Katajisto <t@ktj.st>
2018-01-22 22:37:09 +0200

    Added state system with Game Over and Start screens

--- main.cpp
+++ main.cpp
@@ -10,6 +10,18 @@ int main()
 	Text score;
 	Text boostt;
 	Text st;
+	Text start;
+	Text gameover;
+	start.setFont(gamefont);
+	start.setString("PRESS SPACE TO START");
+	start.setFillColor(Color::Green);
+	start.setCharacterSize(20);
+	start.setPosition(120,200);
+	gameover.setFont(gamefont);
+	gameover.setString("GAME OVER");
+	gameover.setPosition(170,230);
+	gameover.setCharacterSize(20);
+	gameover.setFillColor(Color::Green);
 	st.setFont(gamefont);
 	st.setFillColor(Color::Green);
 	st.setString("SCORE");
@@ -35,19 +47,9 @@ int main()
 	food.setFillColor(Color::Red);
 	food.setPosition(rand() % 489 + 1, rand() % 489 + 1);
 	int framesfromboost = 0;
+	int state = 0;
 	while(window.isOpen())
 	{
-		if(boost != 1)
-		{
-			framesfromboost++;
-			if(framesfromboost >= 100)
-			{
-				framesfromboost = 0;
-				boost = 1;
-			}
-		}
-		score.setString(to_string(snakesz));
-		sleep(seconds(0.05));
 		Event event;
 		while(window.pollEvent(event))
 		{
@@ -56,51 +58,78 @@ int main()
 				window.close();
 			}
 		}
-		if(Keyboard::isKeyPressed(Keyboard::W) && y_mov == 0) {x_mov = 0; y_mov = boost *  -10; goto rdy;}
-		if(Keyboard::isKeyPressed(Keyboard::S) && y_mov == 0) {x_mov = 0; y_mov = boost * 10; goto rdy;}
-		if(Keyboard::isKeyPressed(Keyboard::A) && x_mov == 0) {x_mov = boost * -10; y_mov = 0; goto rdy;}
-		if(Keyboard::isKeyPressed(Keyboard::D) && x_mov == 0) {x_mov = boost * 10; y_mov = 0;}
-		rdy:
-		window.clear(Color::Black);
-		snake.push_back(r);
-		r.move(x_mov, y_mov);
-//		cout << snake.size() << "\n";
-		FloatRect headcollider = r.getGlobalBounds();
-		while(snake.size() > snakesz) 
+		if(state == 0)
 		{
-			snake.pop_front();
-		}
-		for(int i = 0; i < snake.size(); i++)
-		{
-			window.draw(snake[i]);
-		}
-		Vector2f rpos = r.getPosition();
-		if(rpos.x > 499) r.setPosition(0, rpos.y);
-		if(rpos.y > 499) r.setPosition(rpos.x, 0);
-		if(rpos.y < 0) r.setPosition(rpos.x, 489);
-		if(rpos.x < 0) r.setPosition(489, rpos.y);
-		window.draw(score);
-		if(boost != 1)	window.draw(boostt);
-		window.draw(food);
-		window.draw(r);
-		window.draw(st);
-		FloatRect foodCollider = food.getGlobalBounds();
-		if(headcollider.intersects(foodCollider))
-		{
-			boost = 1.5;
-			framesfromboost = 0;
-			snakesz += 3;
-			food.setPosition(rand() % 489 + 1, rand() % 489 + 1);
-		}
-		for(int i = 0; i < snake.size(); i++)
-		{
-			FloatRect tempcollider = snake[i].getGlobalBounds();
-			if(headcollider.intersects(tempcollider))
+			sleep(seconds(0.05));
+			window.clear(Color::Black);
+			window.draw(start);
+			if(Keyboard::isKeyPressed(Keyboard::Space))
 			{
-				sleep(seconds(1.5));
-				window.close();
-				cout << "YOU LOST" << "\n";
+				state = 1;
 			}
+		}
+		if(state == 1)
+		{
+			if(boost != 1)
+			{
+				framesfromboost++;
+				if(framesfromboost >= 100)
+				{
+					framesfromboost = 0;
+					boost = 1;
+				}
+			}
+			score.setString(to_string(snakesz));
+			sleep(seconds(0.05));
+			if(Keyboard::isKeyPressed(Keyboard::W) && y_mov == 0) {x_mov = 0; y_mov = boost *  -10; goto rdy;}
+			if(Keyboard::isKeyPressed(Keyboard::S) && y_mov == 0) {x_mov = 0; y_mov = boost * 10; goto rdy;}
+			if(Keyboard::isKeyPressed(Keyboard::A) && x_mov == 0) {x_mov = boost * -10; y_mov = 0; goto rdy;}
+			if(Keyboard::isKeyPressed(Keyboard::D) && x_mov == 0) {x_mov = boost * 10; y_mov = 0;}
+			rdy:
+			window.clear(Color::Black);
+			snake.push_back(r);
+			r.move(x_mov, y_mov);
+	//		cout << snake.size() << "\n";
+			FloatRect headcollider = r.getGlobalBounds();
+			while(snake.size() > snakesz) 
+			{
+				snake.pop_front();
+			}
+			for(int i = 0; i < snake.size(); i++)
+			{
+				window.draw(snake[i]);
+			}
+			Vector2f rpos = r.getPosition();
+			if(rpos.x > 499) r.setPosition(0, rpos.y);
+			if(rpos.y > 499) r.setPosition(rpos.x, 0);
+			if(rpos.y < 0) r.setPosition(rpos.x, 489);
+			if(rpos.x < 0) r.setPosition(489, rpos.y);
+			window.draw(score);
+			if(boost != 1)	window.draw(boostt);
+			window.draw(food);
+			window.draw(r);
+			window.draw(st);
+			FloatRect foodCollider = food.getGlobalBounds();
+			if(headcollider.intersects(foodCollider))
+			{
+				boost = 1.5;
+				framesfromboost = 0;
+				snakesz += 3;
+				food.setPosition(rand() % 489 + 1, rand() % 489 + 1);
+			}
+			for(int i = 0; i < snake.size(); i++)
+			{
+				FloatRect tempcollider = snake[i].getGlobalBounds();
+				if(headcollider.intersects(tempcollider))
+				{
+					window.draw(gameover);
+					window.display();
+					sleep(seconds(2.5));
+					snake = {};
+					snakesz = 3;
+					state = 0;
+				}
+			}			
 		}
 		window.display();
 	}
